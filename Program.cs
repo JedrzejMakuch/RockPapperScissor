3969700dde23793f66f3e9eadcd3c38e4edb7369
jedrek <jedrek@LAPTOP-HGKA0LEI>
2022-10-02 22:12:33 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace RockPapperScissor
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Services services = new Services();
+            string inputCPU;
+            Random random = new Random();
+            int cpuPoints = 0;
+            int playerPoints = 0;
+            int round = 1;
+            int maxRounds = 5;
+            bool roundsValid;
+
+            while (true)
+            {
+                if (round == 1)
+                {
+                    Console.Write("Hello! How many rounds u want play? ");
+                    roundsValid = Int32.TryParse(Console.ReadLine(), out maxRounds);
+                    if (!roundsValid)
+                    {
+                        Console.WriteLine("Wrong Command");
+                        continue;
+                    }
+                }
+
+                int numCPU = random.Next(0, 3);
+                Console.Write("Round: " + round + ". " + "Choose between ROCK, PAPPER, SCISSORS : ");
+                string inputPlayer = Console.ReadLine();
+
+                Console.WriteLine();
+                switch (numCPU)
+                {
+                    case 0:
+                        inputCPU = "rock";
+                        services.InputCpuRock(inputCPU, ref cpuPoints, ref playerPoints, inputPlayer);
+                        break;
+
+                    case 1:
+                        inputCPU = "scissors";
+                        services.inputCpuScissors(inputCPU, ref cpuPoints, ref playerPoints, inputPlayer);
+                        break;
+
+                    case 2:
+                        inputCPU = "papper";
+                        services.inputCpuPapper(inputCPU, ref cpuPoints, ref playerPoints, inputPlayer);
+                        break;
+                };
+
+                Console.WriteLine();
+                round++;
+                services.Scores(ref cpuPoints, ref playerPoints, ref round, maxRounds);
+            }
+        }
+    }
+}
---
+++ Services.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace RockPapperScissor
+{
+    public class Services
+    {
+        public void inputCpuPapper(string inputCPU, ref int cpuPoints, ref int playerPoints, string inputPlayer)
+        {
+            Console.WriteLine("Computer chose: " + inputCPU);
+            if (inputPlayer.ToLower() == "scissors")
+            {
+                Console.WriteLine("You won!");
+                playerPoints++;
+            }
+            else if (inputPlayer.ToLower() == "papper")
+            {
+                Console.WriteLine("No one win!");
+            }
+            else if (inputPlayer.ToLower() == "rock")
+            {
+                Console.WriteLine("Computer Won!");
+                cpuPoints++;
+            }
+            else
+            {
+                Console.WriteLine("Wrong Command! Point for computer!");
+                cpuPoints++;
+            }
+        }
+
+        public void inputCpuScissors(string inputCPU, ref int cpuPoints, ref int playerPoints, string inputPlayer)
+        {
+            Console.WriteLine("Computer chose: " + inputCPU);
+            if (inputPlayer.ToLower() == "rock")
+            {
+                Console.WriteLine("You won!");
+                playerPoints++;
+            }
+            else if (inputPlayer.ToLower() == "scissors")
+            {
+                Console.WriteLine("No one win!");
+            }
+            else if (inputPlayer.ToLower() == "papper")
+            {
+                Console.WriteLine("Computer Won!");
+                cpuPoints++;
+            }
+            else
+            {
+                Console.WriteLine("Wrong Command! Point for computer!");
+                cpuPoints++;
+            }
+        }
+
+        public void InputCpuRock(string inputCPU, ref int cpuPoints, ref int playerPoints, string inputPlayer)
+        {
+            Console.WriteLine("Computer chose: " + inputCPU);
+            if (inputPlayer.ToLower() == "rock")
+            {
+                Console.WriteLine("No one win!");
+            }
+            else if (inputPlayer.ToLower() == "scissors")
+            {
+                Console.WriteLine("Computer Won!");
+                cpuPoints++;
+            }
+            else if (inputPlayer.ToLower() == "papper")
+            {
+                Console.WriteLine("You Won!");
+                playerPoints++;
+            }
+            else
+            {
+                Console.WriteLine("Wrong Command! Point for computer!");
+                cpuPoints++;
+            }
+        }
+
+        public void Scores(ref int cpuPoints, ref int playerPoints, ref int round, int maxRounds)
+        {
+            if (round > maxRounds)
+            {
+                if (playerPoints > cpuPoints)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Your points: " + playerPoints + ", Computer points: " + cpuPoints);
+                    Console.WriteLine("YOU WON THE GAME!!");
+                }
+                else if (playerPoints < cpuPoints)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your points: " + playerPoints + ", Computer points: " + cpuPoints);
+                    Console.WriteLine("YOU LOSE THE GAME!!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Your points: " + playerPoints + ", Computer points: " + cpuPoints);
+                    Console.WriteLine("DRAW!!");
+                }
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Do you wanna play again? <y/n>");
+
+                PlayAgain(ref cpuPoints, ref playerPoints, ref round);
+            }
+        }
+
+        public void PlayAgain(ref int cpuPoints, ref int playerPoints, ref int round)
+        {
+            var consoleKey = Console.ReadKey(true);
+            if (consoleKey.Key == ConsoleKey.N)
+            {
+                Environment.Exit(0);
+            } 
+            else if(consoleKey.Key == ConsoleKey.Y)
+            {
+                Console.Clear();
+                round = 1;
+                playerPoints = 0;
+                cpuPoints = 0;
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
